6abd2b6b8c1f84addab7fd23e2a97819c934ef1b
Ahmed <ahmedabdullahi083@gmail.com>
2025-11-06 15:36:01 -0800

    form submission block

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,6 +1,7 @@
 const email = document.querySelector('user_email');
 const fname = document.querySelector('fname');
 const errorMsg = document.querySelector('errorMsg');
+const form = document.querySelector('form');
 
 email.addEventListener("input", (event) => {
     if (email.validity.typeMismatch) {
@@ -15,10 +16,15 @@ email.addEventListener("input", (event) => {
 fname.addEventListener("input", (event) => {
     if (fname.validity.patternMismatch) {
         errorMsg.textContent = "Please enter valid characters for your first name.";
-        errorMsg.classList = "hidden";
-        event.preventDefault();
+        errorMsg.classList.remove('hidden');
     }
     else {
         errorMsg.textContent = "";
+    }
+});
+
+form.addEventListener('submit', (event) => {
+    if (fname.validity.patternMismatch){
+        event.preventDefault();
     }
 });
