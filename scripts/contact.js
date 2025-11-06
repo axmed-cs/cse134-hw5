c37db434118bae6782004dac207eb3a734b9a0c5
Ahmed <ahmedabdullahi083@gmail.com>
2025-11-06 14:50:01 -0800

    validity form changes

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,13 +1,23 @@
 const email = document.querySelector('user_email');
-const message = document.querySelector('message');
-const emailError = document.querySelector('user_email_error')
-
+const fname = document.querySelector('fname');
+const errorMsg = document.querySelector('errorMsg');
 
 email.addEventListener("input", (event) => {
-    if (!email.validity.typeMismatch) {
+    if (email.validity.typeMismatch) {
         email.setCustomValidity("I am expecting an email address.");
     }
     else {
         email.setCustomValidity("");
+    }
+});
+
+
+fname.addEventListener("input", (event) => {
+    if (fname.validity.typeMismatch) {
+        errorMsg.textContent = "Please enter valid characters for your first name.";
+        errorMsg.classList = "hidden";
+    }
+    else {
+        errorMsg.textContent = "";
     }
 });
