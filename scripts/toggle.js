a69b04587dcf04eeaf7998daa110465c8abeac07
Ahmed <ahmedabdullahi083@gmail.com>
2025-11-11 11:34:00 -0800

    localStorage dark mode feature

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -3,7 +3,20 @@ const toggle_circle = document.querySelector('.theme-circle');
 const body = document.querySelector('body');
 
 
+const mode = JSON.parse(localStorage.getItem('mode')); 
+
+if (mode == 'dark'){
+    body.classList.add('dark');
+    toggle_circle.classList.add('dark');
+} 
+
 toggle.addEventListener('click', () => {
     toggle_circle.classList.toggle('dark');
     body.classList.toggle('dark');
+
+    if (body.classList.contains('dark')) {
+        localStorage.setItem('mode', 'dark');
+    } else {
+        localStorage.setItem('mode', 'light');
+    }
 }); 
